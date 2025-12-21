f3e4cabb2e996e556cc6db7f9b0119ab45801ce8
Cisse-Mohamed <65600284+Cisse-Mohamed@users.noreply.github.com>
2025-12-21 17:15:36 +0000

    agin

--- apps/videoconference/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/video/regular/(?P<session_slug>[\w-]+)/(?:breakout/(?P<breakout_slug>[\w-]+)/)?$', consumers.VideoCallConsumer.as_asgi(), {'session_type': 'regular'}),
-    re_path(r'ws/video/instant/(?P<session_slug>[\w-]+)/$', consumers.VideoCallConsumer.as_asgi(), {'session_type': 'instant'}),
-]
