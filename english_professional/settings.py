ad18d6c31b1e6720170be40b357656372bfd2efd
Cisse-Mohamed <65600284+Cisse-Mohamed@users.noreply.github.com>
2025-12-22 13:04:17 +0000

    oklam

--- english_professional/settings.py
+++ english_professional/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', '203.0.113.0']
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', '203.0.113.0', "127.0.0.1"]
 
 
 
